f188f750df3c0d09fcab7b591ccdc2feb83bf3c6
trandangquang <trandangquang2203@gmail.com>
2023-10-10 16:28:30 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const  mongoose  = require("mongoose");
-const routes = require("./routes");
-const cors = require('cors')
-const bodyParser = require("body-parser");
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3001
+const app = express();
+const port = process.env.PORT || 3001;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 routes(app);
 
-mongoose.connect(`${process.env.MONGO_DB}`)
-.then(()=>{
-  console.log('Connect DB success')
-}).catch((err)=>{
-  console.log(err)
-})
+mongoose
+  .connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+    console.log('Connect DB success');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(port, ()=> {
-  console.log('Server is running in port:', + port)
-})
+app.listen(port, () => {
+  console.log('Server is running in port:', +port);
+});
